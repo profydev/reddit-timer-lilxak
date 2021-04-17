02d5218699c2c644528d5484e92f553974f6c003
lilxak <zakben45@gmail.com>
2021-04-17 17:20:18 +0000

    Add javascript as default value to Search nav link

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ function Header() {
           <img className="header-logo" src="/logo.svg" alt="Reddit Timer" />
         </Link>
         <div className="header-nav-links">
-          <Link to="/search"> Search </Link>
+          <Link to="/search?sub=javascript"> Search </Link>
           <Link to="/#how-it-works"> How it works </Link>
           <Link to="/#about"> About </Link>
         </div>
