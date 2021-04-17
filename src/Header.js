e5cb9df58cfac42b689b2b39db6693c6e6dd28f3
lilxak <zakben45@gmail.com>
2021-04-17 16:01:23 +0000

    Add logo to header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      This is a header
+      <nav>
+        <a href="/">
+          <img src="/logo.svg" alt="Reddit Timer" />
+        </a>
+      </nav>
     </header>
   );
 }
